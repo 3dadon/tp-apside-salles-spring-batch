1c1050c1dab9f6deda4d0ddfed4a3aaeb0a416f6
Paul Henri LARBAT <paul-henri.larbat@apside-groupe.com>
2023-08-28 14:05:20 +0200

    Conf + retrait spring web

--- src/main/java/com/jobi_jobatch/JobiJobatchApplication.java
+++ src/main/java/com/jobi_jobatch/JobiJobatchApplication.java
@@ -1,14 +1,15 @@
 package com.jobi_jobatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableBatchProcessing
 public class JobiJobatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobiJobatchApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(JobiJobatchApplication.class, args)));
 	}
 }
